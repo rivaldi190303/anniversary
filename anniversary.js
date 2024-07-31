cae34cb4b3e5368b183da0a501e953e85b0ce2fb
rivaldi190303 <113177773+rivaldi190303@users.noreply.github.com>
2024-07-31 16:31:07 +0700

    Update anniversary.js

--- anniversary.js
+++ anniversary.js
@@ -109,6 +109,6 @@ function kirimSaran() {
       // Menambahkan data baru ke array
       existingSaran.push(saran);
       localStorage.setItem('saran', JSON.stringify(existingSaran));
-      alert('I LOVE YOU FOREVER LAURA, Even you're not💕');
+      alert('I LOVE YOU FOREVER LAURA💕');
     }
     ;
